3bc6ec957c581f252c45316fbde3ffb7e64e5a5c
VasylK6178 <ksvsl6178@ukr.net>
2021-07-21 22:55:14 +0300

    initial

---
+++ JS.js
@@ -0,0 +1,19 @@
+
+
+
+// - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+//
+function nambers (a, b) {
+    for (let i=0; i<b; i+=6) {
+        console.log(i)
+    }
+}
+nambers(0, 100 )
+
+// class Nambers {
+//     constructor( a, b) {
+//         this.namb = a;
+//
+//     }
+//
+// }
